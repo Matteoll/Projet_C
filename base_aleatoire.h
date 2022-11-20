16c63bf6a5229fe1ff4b53f5fd94149bd846f093
Matteoll <70729143+Matteoll@users.noreply.github.com>
2022-11-21 00:56:19 +0100

    Add files via upload

---
+++ base_aleatoire.h
@@ -0,0 +1,20 @@
+//
+// Created by cambo on 13/11/2022.
+//
+
+#ifndef GENERATEUR_DE_PHRASE_BASE_ALEATOIRE_H
+#define GENERATEUR_DE_PHRASE_BASE_ALEATOIRE_H
+
+
+#include "rechercher_mot.h"
+
+
+
+
+
+
+void genererPhrase(t_verb_tree*, t_NA_tree*, t_NA_tree*, t_adverbe_tree*, char*, long);
+
+
+
+#endif //GENERATEUR_DE_PHRASE_BASE_ALEATOIRE_H
